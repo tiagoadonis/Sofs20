0fc5b0df50b30c4d017a5cf5ce9da88174a07704
tiagoadonis <tiago.adonis@ua.pt>
2020-11-16 11:51:38 +0000

    grp_get_fileblock.cpp | script to test it

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -106,3 +106,38 @@ function tt-rrc()
     
     echo -ne "rrc\nq\n" | tt -q1 -p0-0 -b $@
 }
+# --------------------------------------------------------------------------
+# To test the get file block function
+function tt-gfb()
+{
+    echo -ne "ai\n1\n555\nq\ne" | tt -q1 -p0-0 -b
+
+    for i in $(seq 0 4)
+    do
+        echo -ne "afb\n1\n$i\nq\ne" | tt -q1 -p0-0 -b 
+    done
+
+    for index in 260 516 772
+    do
+        echo -ne "afb\n1\n$index\nq\ne" | tt -q1 -p0-0 -b
+    done
+
+    # Calling get file block function
+    # Direct zone
+    for j in $(seq 0 3)
+    do
+        echo -ne "gfb\n1\n$j\nq\ne" | tt -q1 -p0-0 -b $@
+    done
+
+    # Indirect zone
+    for k in 4 260 516
+    do
+        echo -ne "gfb\n1\n$k\nq\ne" | tt -q1 -p0-0 -b $@
+    done
+
+    # 2x Indirect zone
+    for l in 772
+    do
+        echo -ne "gfb\n1\n$l\nq\ne" | tt -q1 -p0-0 -b $@
+    done
+}
