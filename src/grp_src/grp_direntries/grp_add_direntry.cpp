328094f25695cdc6d1cb4d913e0551fb7e8b2af1
João <jtsimoes@ua.pt>
2020-12-06 00:56:50 +0000

    Finished AddDirentry function

--- src/grp_src/grp_direntries/grp_add_direntry.cpp
+++ src/grp_src/grp_direntries/grp_add_direntry.cpp
@@ -16,8 +16,46 @@ namespace sofs20
     {
         soProbe(202, "%s(%d, %s, %u)\n", __FUNCTION__, pih, name, cin);
 
+        SOInode* inodep = soGetInodePointer(pih);
+        unsigned block_index = BlockNullReference;
+        unsigned direntry_index = BlockNullReference;
+
+        for (unsigned i = 0; i < inodep->size/BlockSize; i++){
+            SODirentry direntry[DPB];
+            soReadFileBlock(pih, i, direntry);
+            
+            for (unsigned j = 0; j < DPB; j++){
+                if (block_index == BlockNullReference && direntry[j].name[0] == '\0'){
+                    block_index = i;
+                    direntry_index = j;
+                }
+            }
+        }
+
+        SODirentry direntry[DPB];
+
+        if (block_index == BlockNullReference){
+            direntry_index = 0;
+            block_index = inodep->size/BlockSize;
+            soAllocFileBlock(pih, block_index);
+            inodep->size += BlockSize;
+
+            for (unsigned i = 0; i < DPB; i++){
+                strcpy(direntry[i].name, "");
+                direntry[i].in = InodeNullReference;
+            }
+        }
+        else{
+            soReadFileBlock(pih, block_index, direntry);
+        } 
+
+        strcpy(direntry[direntry_index].name, name);
+        direntry[direntry_index].in = cin;
+
+        soWriteFileBlock(pih, block_index, direntry);
+
         /* replace the following line with your code */
-        binAddDirentry(pih, name, cin);
+        //binAddDirentry(pih, name, cin);
     }
 };
 
